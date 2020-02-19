cc7685723b1024ab3a8010b7ba920367c53f2983
Sherry <huizezhangsh@gmail.com>
2020-02-19 15:05:47 +1100

    first documentation

---
+++ R/categories.R
@@ -0,0 +1,4 @@
+#' @title valid quick draw category
+#' @description a vector of valid quick draw category
+#' @source [https://raw.githubusercontent.com/googlecreativelab/quickdraw-dataset/master/categories.txt]
+"categories"
