ab1d50fe10d028af1f21d38c6f6d918229468629
Sherry <huizezhangsh@gmail.com>
2020-02-20 11:03:23 +1100

    qd_sample

---
+++ R/qd_sample.R
@@ -0,0 +1,19 @@
+#'@title qd_sample
+#'@param categories a vector of valid QuickDraw category
+#'@param size a vector of number of rows to take from each category
+#'@details qd_sample() downloads QuickDraw data from multiple categories and take a sample from each category
+#'@return A tibble object that binds the sample data from all catgories.
+#'@examples
+#'qd_sampe(c("octopus", "star"), 5)
+#'@export
+
+qd_sample <- function(categories, size){
+  stopifnot(length(categories) %% length(size) == 0)
+  stopifnot(categories %in% qd_categories())
+
+  sample_one <- function(category, size){
+    qd_read(category) %>% dplyr::sample_n(size)
+  }
+
+  purrr::map2_df(categories, size, sample_one)
+}
